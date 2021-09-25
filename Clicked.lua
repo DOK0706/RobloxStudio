5349c925753a1131004813a15c314933ae4f274e
도균 <90293526+DOK0706@users.noreply.github.com>
2021-09-25 23:48:40 +0900

    Create Clicked.lua
    클릭 판정

---
+++ Clicked.lua
@@ -0,0 +1,21 @@
+function onClicked()
+	for i,v in pairs (game.Workspace:GetChildren()) do
+	if v.Name == (script.Parent.Parent.Parent.name1.Value) then
+v.Body.Hinge.Motor6D.DesiredAngle = -0.5
+v.Holder.Connect.Motor6D.DesiredAngle = 0.5
+	end
+	end
+	end
+ 
+script.Parent.MouseButton1Down:connect(onClicked)
+
+function onClicked()
+	for i,v in pairs (game.Workspace:GetChildren()) do
+	if v.Name == (script.Parent.Parent.Parent.name2.Value) then
+v.Body.Hinge.Motor6D.DesiredAngle = 0.5
+v.Holder.Connect.Motor6D.DesiredAngle = 0.5
+	end
+	end
+	end
+ 
+script.Parent.MouseButton1Down:connect(onClicked)

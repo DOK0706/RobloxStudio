c9542e6d015a288b12fcaf0e2663122112bc88f9
도균 <90293526+DOK0706@users.noreply.github.com>
2021-09-26 15:57:25 +0900

    Create Head.lua
    머리 장식품 (모자, 안경 등)

---
+++ Head.lua
@@ -0,0 +1,25 @@
+debounce = true
+
+function onTouched(hit)
+	if (hit.Parent:findFirstChild("Humanoid") ~= nil and debounce == true) then
+		debounce = false
+		h = Instance.new("Hat")
+		p = Instance.new("Part")
+		h.Name = "LongHairHeadBand Black"
+		p.Parent = h
+		p.Position = hit.Parent:findFirstChild("Head").Position
+		p.Name = "Handle" 
+		p.formFactor = 0
+		p.Size = Vector3.new(2, 1, 1) 
+		p.BottomSurface = 0 
+		p.TopSurface = 0 
+		p.Locked = true 
+		script.Parent.Mesh:clone().Parent = p
+		h.Parent = hit.Parent
+		h.AttachmentPos = Vector3.new(0, 0.8, 0)
+		wait(5)
+		debounce = true
+	end
+end
+
+script.Parent.Touched:connect(onTouched)

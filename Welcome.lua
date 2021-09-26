d851b70c71c7c68a934026b7b2a6bd5381c9b068
도균 <90293526+DOK0706@users.noreply.github.com>
2021-09-26 15:56:57 +0900

    Create Welcome.lua
    안내창

---
+++ Welcome.lua
@@ -0,0 +1,6 @@
+local PlayerName = game.Players.LocalPlayer.Name
+local GameName = game.Name
+local CreatorName = game.CreatorId
+
+script.Parent.Title1.Text = 'Hello, '..PlayerName..'!'
+script.Parent.Title2.Text = 'Welcome to '..GameName..'!'

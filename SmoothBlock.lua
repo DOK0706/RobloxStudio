7f766c04336ca7b8c03130ad650d43343614147a
도균 <90293526+DOK0706@users.noreply.github.com>
2021-09-26 16:00:48 +0900

    Create SmoothBlock.lua
    안티앨리어싱(?)

---
+++ SmoothBlock.lua
@@ -0,0 +1,55 @@
+print("glow loaded") 
+
+local light = script.Parent 
+
+while true do 
+
+		light.Transparency = 0
+	
+		wait(1) 
+		
+		light.Transparency = 0.5
+	
+		wait(1)
+
+		light.Transparency = 1
+
+		wait(1) 
+		
+		light.Transparency = 0
+	
+		wait(1) 
+		
+		light.Transparency = 0.5
+	
+		wait(1)
+
+		light.Transparency = 1
+
+		wait(1) 
+		
+		light.Transparency = 0
+	
+		wait(1) 
+		
+		light.Transparency = 0.5
+	
+		wait(1)
+
+		light.Transparency = 1
+
+		wait(1) 
+		
+		light.Transparency = 0
+	
+		wait(1) 
+		
+		light.Transparency = 0.5
+	
+		wait(1)
+
+		light.Transparency = 1
+
+		wait(1) 
+
+end 

54824377a96f1ff89475757423b39df045bbd23e
도균 <90293526+DOK0706@users.noreply.github.com>
2021-10-03 22:41:26 +0900

    Create TvMusic.lua

---
+++ TvMusic.lua
@@ -0,0 +1,33 @@
+	0: rbxassetid://702875426 \\ No signal
+
+	1: rbxassetid://1837889876 \\ Cry - APM Music
+	
+	2: rbxassetid://1846537079 \\ Cry For More (A) - APM Music
+	
+	3: rbxassetid://588711502 \\ Take on oof -(idk who made this)
+	
+	4: rbxassetid://516766251 \\ Macintosh Plus - PrismCorp Virtual Enterprises
+
+	5: rbxassetid://1696854181 \\ Delinquent Theme - Simon Amiot
+
+	6: rbxassetid://6729259949 \\ Get Got (Instrumental) - Death Grips
+	
+	7: rbxassetid://1876308078 \\ Lilium - Kumiko Noma, Yukio Kondo and Kayo Konoshi
+	
+		-Images-
+		
+	0: rbxassetid://25067669 \\ No Signal
+
+	1: rbxassetid://1365494765 \\ Galaxy
+
+	2: rbxassetid://1047349259 \\ Night Time City
+	
+	3: rbxassetid://1408842771 \\ Despacito Spider
+	
+	4: rbxassetid://4432302999 \\ Macintosh Plus - Floral Shoppe
+	
+	5: rbxassetid://6919659360 \\ Red Forest
+	
+	6: rbxassetid://223017964 \\ MC Ride
+	
+	7: rbxassetid://1206367650 \\ Kaede / Lucy
